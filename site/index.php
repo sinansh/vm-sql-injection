50b2903bc06d6265c9d83c414d5b84682dd03132
sinansahin <bote.sinansahin@gmail.com>
2016-07-13 19:11:46 +0300

    .

---
+++ site/index.php
@@ -0,0 +1,32 @@
+﻿<?php
+$lnk = mysql_connect("localhost", "londravm_A", "londravm_A");
+$db = mysql_select_db("", $lnk);
+if(!preg_match('/^[0-9]+/', $_GET["id"])){
+  die("int değer gönder hacı :D ");
+}
+$sql =  "Select * From users where id=";
+$sql .= $_GET["id"];
+
+$result =  mysql_query($sql);
+
+if($result) {
+?>
+  <table class="table table-hover">
+    <tr>
+      <th>id</th>
+      <th>name</th>
+      <th>username</th>
+    </tr>
+  </table>
+<?php
+while($row = mysql_fetch_assoc($result)){
+  echo "<tr>";
+  echo "<td>".$row['id']."</td>";
+  echo "<td>".$row['name']."</td>";
+  echo "<td>".$row['username']."</td>";
+  echo "</tr>";
+}
+}
+require_once("footer.php");
+
+?>
